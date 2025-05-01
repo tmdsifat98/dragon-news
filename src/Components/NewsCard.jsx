a982ec8ac89055f0a270d224ac1e648f3bec86c6
sifat tarafder <tmdsifat98@gmail.com>
2025-05-01 13:57:16 +0600

    news section added

--- src/Components/NewsCard.jsx
+++ src/Components/NewsCard.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import { useParams } from 'react-router';
-
+import React, { use, useEffect, useState } from "react";
+import { useParams } from "react-router";
+import SingleNews from "./SingleNews";
+const newsPromise = fetch("/news.json").then((res) => res.json());
 const NewsCard = () => {
-    const {id} = useParams()
-    console.log(id)
-    return (
-        <div>
-            NewsCard
-        </div>
-    );
+  const [newsCard, setNewsCard] = useState([]);
+  const news = use(newsPromise);
+  const { id } = useParams();
+  useEffect(() => {
+    if (id == "0") return setNewsCard(news);
+    const breakingNews = news.filter((n) => n.others.is_today_pick == true);
+    if (id == "1") return setNewsCard(breakingNews);
+    const filteredNews = news.filter((n) => n.category_id == id);
+    setNewsCard(filteredNews);
+  }, [id, news]);
+  return <div>{newsCard.map(singleNews=><SingleNews key={singleNews.id} singleNews={singleNews}/>)}</div>;
 };
 
 export default NewsCard;
